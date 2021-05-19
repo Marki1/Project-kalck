633347d37fbda9739eaeaf220a7ae7d7c83df3f9
Marki1 <57572497+Marki1@users.noreply.github.com>
2021-05-19 11:27:19 -0300

    Add files via upload

---
+++ project_calc-main/js/kalcWeb.js
@@ -0,0 +1,76 @@
+//DOM
+
+//Números
+const um = document.getElementById("um");
+const dois = document.getElementById("dois");
+const tres = document.getElementById("tres");
+const quatro = document.getElementById("quatro");
+const cinco = document.getElementById("cinco");
+const seis = document.getElementById("seis");
+const sete = document.getElementById("sete");
+const oito = document.getElementById("oito");
+const nove = document.getElementById("nove");
+const zero = document.getElementById("zero");
+const ponto = document.getElementById("ponto");
+const limpar = document.getElementById("limpar");
+
+//Operações
+const dividir = document.getElementById("dividir");
+const multiplicar = document.getElementById("multiplicar");
+const subtrair = document.getElementById("subtrair");
+const somar = document.getElementById("somar");
+const igual = document.getElementById("igual");
+
+//Eventos
+um.onclick = function() {
+    console.log(um.innerText);
+};
+dois.onclick = function() {
+    console.log(dois.innerText);
+};
+tres.onclick = function() {
+    console.log(tres.innerText);
+};
+quatro.onclick = function() {
+    console.log(quatro.innerText);
+};
+cinco.onclick = function() {
+    console.log(cinco.innerText);
+};
+seis.onclick = function() {
+    console.log(seis.innerText);
+};
+sete.onclick = function() {
+    console.log(sete.innerText);
+};
+oito.onclick = function() {
+    console.log(oito.innerText);
+};
+nove.onclick = function() {
+    console.log(nove.innerText);
+};
+zero.onclick = function() {
+    console.log(zero.innerText);
+};
+ponto.onclick = function() {
+    console.log(ponto.innerText);
+};
+limpar.onclick = function() {
+    console.log(limpar.innerText);
+};
+
+dividir.onclick = function() {
+    console.log(dividir.innerText);
+};
+multiplicar.onclick = function() {
+    console.log(multiplicar.innerText);
+};
+subtrair.onclick = function() {
+    console.log(subtrair.innerText);
+};
+somar.onclick = function() {
+    console.log(somar.innerText);
+};
+igual.onclick = function() {
+    console.log(igual.innerText);
+};
